d4d221d9a8b9074984f0552ab40e5da54addacfc
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-16 10:45:01 -0700

    implemented assertArraysEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,10 @@
+const eqArrays = function(actual, expected) {
+  return actual.every((elm, index) => elm === expected[index]);
+};
+
+const assertArraysEqual = function(actual, expected) {
+  const passed = `\u2705 Assertion Passed: [${actual}] === [${expected}]`;
+  const failed = `\u{1F534} Assertion Failed: [${actual}] !== [${expected}]`;
+
+  return eqArrays(actual, expected) ? passed : failed;
+};
