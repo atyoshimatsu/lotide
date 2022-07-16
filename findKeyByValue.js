ef7eb4d842804c503f73f2096c2afd3979daa082
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-16 15:58:37 -0700

    implemeted findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  const passed = `\u2705 Assertion Passed: [${actual}] === [${expected}]`;
+  const failed = `\u{1F534} Assertion Failed: [${actual}] !== [${expected}]`;
+
+  return actual === expected ? passed : failed;
+};
+
+const findKeyByValue = function(obj, value) {
+  for (const key of Object.keys(obj)) {
+    if (obj[key] === value) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
