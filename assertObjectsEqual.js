ab1304ee2773a8cd6dbc44e4c6989de72a3f5f0a
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-16 23:45:21 +0000

    excuted lint

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  const passed = `\u2705 Assertion Passed: [${actual}] === [${expected}]`;
-  const failed = `\u{1F534} Assertion Failed: [${actual}] !== [${expected}]`;
-
-  return actual === expected ? passed : failed;
-};
-
 const eqArrays = function(actual, expected) {
   if (!Array.isArray(actual) || !Array.isArray(expected)) {
     return false;
