55e4a3de13c2d79d7a970c9f6d3e696bd8012e50
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-16 16:25:25 -0700

    implemented eqObjects function

---
+++ eqObjects.js
@@ -0,0 +1,43 @@
+const assertEqual = function(actual, expected) {
+  const passed = `\u2705 Assertion Passed: [${actual}] === [${expected}]`;
+  const failed = `\u{1F534} Assertion Failed: [${actual}] !== [${expected}]`;
+
+  return actual === expected ? passed : failed;
+};
+
+const eqArrays = function(actual, expected) {
+  if (!Array.isArray(actual) || !Array.isArray(expected)) {
+    throw new Error('The argument is not Array');
+  }
+
+  return actual.every((elm, index) => elm === expected[index]);
+};
+
+const eqObjects = function(obj1, obj2) {
+  const keys1 = Object.keys(obj1);
+  const keys2 = Object.keys(obj2);
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  return keys1.every(key => Array.isArray(obj1[key])
+    ? eqArrays(obj1[key], obj2[key])
+    : assertEqual(obj1[key], obj2[key]));
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+console.log(eqObjects(ab, ba)); // => true
+
+const abc = { a: "1", b: "2", c: "3" };
+console.log(eqObjects(ab, abc)); // => false
+
+const abd = { a: "1", b: "2", d: "3" };
+console.log(eqObjects(abc, abd)); // => false
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+console.log(eqObjects(cd, dc)); // => true
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+console.log(eqObjects(cd, cd2)); // => false
