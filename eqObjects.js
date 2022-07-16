18ed6980d46760ea963aaf2ad43e6f15c8978eef
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-16 16:32:28 -0700

    modified eqArray to return false if arguments are not array or arrays length is not equal

--- eqObjects.js
+++ eqObjects.js
@@ -7,7 +7,11 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(actual, expected) {
   if (!Array.isArray(actual) || !Array.isArray(expected)) {
-    throw new Error('The argument is not Array');
+    return false;
+  }
+
+  if (actual.length !== expected.length) {
+    return false;
   }
 
   return actual.every((elm, index) => elm === expected[index]);
--- tail.js
+++ tail.js
@@ -1,6 +1,10 @@
 const eqArrays = function(actual, expected) {
   if (!Array.isArray(actual) || !Array.isArray(expected)) {
-    throw new Error('The argument is not Array');
+    return false;
+  }
+
+  if (actual.length !== expected.length) {
+    return false;
   }
 
   return actual.every((elm, index) => elm === expected[index]);
