ac66359b0b683fd5d255b3d3b85f37e26663c808
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-16 19:39:48 +0000

    implemented flatten function

---
+++ flatten.js
@@ -0,0 +1,25 @@
+const eqArrays = function(actual, expected) {
+  if (!Array.isArray(actual) || !Array.isArray(expected)) {
+    throw new Error('The argument is not Array');
+  }
+
+  return actual.every((elm, index) => elm === expected[index]);
+};
+
+const assertArraysEqual = function(actual, expected) {
+  const passed = `\u2705 Assertion Passed: [${actual}] === [${expected}]`;
+  const failed = `\u{1F534} Assertion Failed: [${actual}] !== [${expected}]`;
+
+  return eqArrays(actual, expected) ? passed : failed;
+};
+
+const flatten = function(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error('The argument is not array');
+  }
+
+  return arr.reduce((prv, cur) => Array.isArray(cur) ? [...prv, ...cur] : [...prv, cur], []);
+};
+
+console.log(assertArraysEqual(flatten([[1, 2], [3, 4], [5], [6]]), [1, 2, 3, 4, 5, 6]));
+console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]));
