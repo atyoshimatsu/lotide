5e83cb2d68267daea75a1855c6edd26e6eef027e
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-16 11:31:19 -0700

    modified eqArrays so it return error if arguments are not array

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,4 +1,8 @@
 const eqArrays = function(actual, expected) {
+  if (!Array.isArray(actual) || !Array.isArray(expected)) {
+    throw new Error('The argument is not Array');
+  }
+
   return actual.every((elm, index) => elm === expected[index]);
 };
 
--- eqArrays.js
+++ eqArrays.js
@@ -1,3 +1,7 @@
 const eqArrays = function(actual, expected) {
+  if (!Array.isArray(actual) || !Array.isArray(expected)) {
+    throw new Error('The argument is not Array');
+  }
+
   return actual.every((elm, index) => elm === expected[index]);
 };
