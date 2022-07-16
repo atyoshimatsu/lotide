47598aa8879ad060ca7070a425a0314338b4780e
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-16 15:12:27 -0700

    modified assertEqual function to compare only primitive types

--- head.js
+++ head.js
@@ -2,11 +2,7 @@ const assertEqual = function(actual, expected) {
   const passed = `\u2705 Assertion Passed: [${actual}] === [${expected}]`;
   const failed = `\u{1F534} Assertion Failed: [${actual}] !== [${expected}]`;
 
-  const result = Array.isArray(actual)
-    ? actual.every((elm, index) => elm === expected[index])
-    : actual === expected;
-
-  return result ? passed : failed;
+  return actual === expected ? passed : failed;
 };
 
 const head = function(arr) {
--- tail.js
+++ tail.js
@@ -1,12 +1,16 @@
-const assertEqual = function(actual, expected) {
+const eqArrays = function(actual, expected) {
+  if (!Array.isArray(actual) || !Array.isArray(expected)) {
+    throw new Error('The argument is not Array');
+  }
+
+  return actual.every((elm, index) => elm === expected[index]);
+};
+
+const assertArraysEqual = function(actual, expected) {
   const passed = `\u2705 Assertion Passed: [${actual}] === [${expected}]`;
   const failed = `\u{1F534} Assertion Failed: [${actual}] !== [${expected}]`;
 
-  const result = Array.isArray(actual)
-    ? actual.every((elm, index) => elm === expected[index])
-    : actual === expected;
-
-  return result ? passed : failed;
+  return eqArrays(actual, expected) ? passed : failed;
 };
 
 const tail = function(arr) {
