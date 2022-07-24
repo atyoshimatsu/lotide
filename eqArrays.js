361c18d710dba0314abf8ad8b309723b22d6fd9c
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-24 04:12:11 +0000

    applied recursion to eqArray

--- eqArrays.js
+++ eqArrays.js
@@ -7,5 +7,15 @@ const eqArrays = function(actual, expected) {
     return false;
   }
 
-  return actual.every((elm, index) => elm === expected[index]);
+  return actual.map((elm, index) => {
+    if (Array.isArray(elm)) {
+      return eqArrays(elm, expected[index]);
+    }
+    return elm === expected[index];
+  }).every(elm => elm === true);
 };
+
+console.log(eqArrays([[2, 3], [4], 6], [[2, 3], [4], 6])); // => true
+console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
+console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false;
+console.log(eqArrays(1, 1)); // => false;
