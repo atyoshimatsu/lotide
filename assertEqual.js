271295687c81c2294bcdedd30fa11269ffa30c27
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-24 16:35:36 -0700

    added tests to assertEqual.js

--- assertEqual.js
+++ assertEqual.js
@@ -4,3 +4,9 @@ const assertEqual = function(actual, expected) {
 
   return actual === expected ? passed : failed;
 };
+
+console.log(assertEqual(0, 0));
+console.log(assertEqual(0, null));
+console.log(assertEqual(1, undefined));
+console.log(assertEqual('test', 'test'));
+console.log(assertEqual(true, true));
