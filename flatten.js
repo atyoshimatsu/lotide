3773aa87de20eb62d0cb750f8d78e1c4cac87bd8
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-24 17:41:08 -0700

    modified flatten as to work for nested array

--- flatten.js
+++ flatten.js
@@ -5,9 +5,10 @@ const flatten = function(arr) {
     throw new Error('The argument is not array');
   }
 
-  return arr.reduce((prv, cur) => Array.isArray(cur) ? [...prv, ...cur] : [...prv, cur], []);
+  return arr.reduce((prv, cur) => Array.isArray(cur) ? [...prv, ...flatten(cur)] : [...prv, cur], []);
 };
 
 console.log('flatten tests --');
 console.log(assertArraysEqual(flatten([[1, 2], [3, 4], [5], [6]]), [1, 2, 3, 4, 5, 6]));
 console.log(assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]));
+console.log(assertArraysEqual(flatten([1, 2, [3, 4, [4, 4]], 5, [6]]), [1, 2, 3, 4, 4, 4, 5, 6]));
