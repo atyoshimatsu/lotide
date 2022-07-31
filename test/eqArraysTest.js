851aee7e423db6c811cfd1cf42298e687566b320
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-31 13:15:38 -0700

    modified eqArraysTest as to use mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,7 +1,20 @@
+const { assert } = require('chai');
 const eqArrays = require('../eqArrays');
 
-console.log('\neqArrays tests --');
-console.log(eqArrays([[2, 3], [4], 6], [[2, 3], [4], 6])); // => true
-console.log(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]])); // => false
-console.log(eqArrays([[2, 3], [4]], [[2, 3], 4])); // => false;
-console.log(eqArrays(1, 1)); // => false;
+describe('eqArrays test', () => {
+  it('should return true for [[2, 3], [4], 6], [[2, 3], [4], 6]', () => {
+    assert.isTrue(eqArrays([[2, 3], [4], 6], [[2, 3], [4], 6]));
+  });
+
+  it('should return false for [[2, 3], [4]], [[2, 3], [4, 5]]', () => {
+   assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]));
+  });
+
+  it('should false for [[2, 3], [4]], [[2, 3], 4]]', () => {
+    assert.isFalse(eqArrays([[2, 3], [4]], [[2, 3], 4]));
+  });
+
+  it('should false passed for 1, 1', () => {
+    assert.isFalse(eqArrays(1, 1));
+  });
+});
