2ad3ca1c95aa146774a275ae84782d2f22c2091a
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-31 14:08:20 -0700

    modified test case names of headTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -6,19 +6,19 @@ describe('middle test', () => {
     assert.deepEqual(middle([]), []);
   });
 
-  it('should return [0] for []', () => {
+  it('should return [] for [0]', () => {
     assert.deepEqual(middle([0]), []);
   });
 
-  it('should return [0, 1] for []]', () => {
+  it('should return [] for [0, 1]]', () => {
     assert.deepEqual(middle([0, 1]), []);
   });
 
-  it('should return [1, 2, 3, 4, 5] for [3]', () => {
+  it('should return [3] for [1, 2, 3, 4, 5]', () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
 
-  it('should return [1, 2, 3, 4, 5, 6] for [3, 4]', () => {
+  it('should return [3, 4] for [1 ,2, 3, 4, 5, 6]', () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   });
 });
