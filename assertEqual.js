82b801174def619e596cdcb51957b3a7d9df319a
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-31 13:09:16 -0700

    modified assertEqualTest as to use mocha and chai

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 const assertEqual = function(actual, expected) {
-  const passed = `\u2705 Assertion Passed: [${actual}] === [${expected}]`;
-  const failed = `\u{1F534} Assertion Failed: [${actual}] !== [${expected}]`;
+  const passed = `\u2705 Assertion Passed: ${actual} === ${expected}`;
+  const failed = `\u{1F534} Assertion Failed: ${actual} !== ${expected}`;
 
   return actual === expected ? passed : failed;
 };
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,8 +1,29 @@
+const { assert } = require('chai');
 const assertEqual = require('../assertEqual');
 
-console.log('\nassertEqual tests --');
-console.log(assertEqual(0, 0));
-console.log(assertEqual(0, null));
-console.log(assertEqual(1, undefined));
-console.log(assertEqual('test', 'test'));
-console.log(assertEqual(true, true));
+describe('assertEqual test', () => {
+  it('should return passed for 0 and 0', () => {
+    const passed = '\u2705 Assertion Passed: 0 === 0';
+    assert.deepEqual(assertEqual(0, 0), passed);
+  });
+
+  it('should return failed for 0 and null', () => {
+    const failed = `\u{1F534} Assertion Failed: 0 !== null`;
+    assert.deepEqual(assertEqual(0, null), failed);
+  });
+
+  it('should return failed for 1 and undefined]', () => {
+    const failed = `\u{1F534} Assertion Failed: 1 !== undefined`;
+    assert.deepEqual(assertEqual(1, undefined), failed);
+  });
+
+  it('should return passed for "test" and "test"', () => {
+    const passed = '\u2705 Assertion Passed: test === test';
+    assert.deepEqual(assertEqual('test', 'test'), passed);
+  });
+
+  it('should return passed for true and true', () => {
+    const passed = '\u2705 Assertion Passed: true === true';
+    assert.deepEqual(assertEqual(true, true), passed);
+  });
+});
