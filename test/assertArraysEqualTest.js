bfe2e00194769b0c6328ab6a5970d77832075d60
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-07-31 14:01:53 -0700

    modified assertArraysEqual as to use mocha and chai

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,6 +1,19 @@
+const { assert } = require('chai');
 const assertArraysEqual = require('../assertArraysEqual');
 
-console.log('\nassertArraysEqual tests --');
-console.log(assertArraysEqual([], []));
-console.log(assertArraysEqual([1, 2, 3], [2, 3]));
-console.log(assertArraysEqual([1, [2, 3, [4]]], [1, [2, 3, [4]]]));
+describe('assertArraysEqual test', () => {
+  it('should return passed for [], []', () => {
+    const passed = '\u2705 Assertion Passed: [] === []';
+    assert.strictEqual(assertArraysEqual([], []), passed);
+  });
+
+  it('should return failed for [1, 2, 3], [2, 3]', () => {
+    const failed = `\u{1F534} Assertion Failed: [1,2,3] !== [2,3]`;
+    assert.strictEqual(assertArraysEqual([1, 2, 3], [2, 3]), failed);
+  });
+
+  it('should return passed for [1, [2, 3, [4]]], [1, [2, 3, [4]]]]', () => {
+    const passed = `\u2705 Assertion Passed: [1,2,3,4] === [1,2,3,4]`;
+    assert.strictEqual(assertArraysEqual([1, [2, 3, [4]]], [1, [2, 3, [4]]]), passed);
+  });
+});
