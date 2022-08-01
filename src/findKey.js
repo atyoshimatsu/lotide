94ce21af7cc0ab2404d9246576fa89055ec54f17
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-08-01 12:12:17 -0700

    implemented findKeyTest

--- src/findKey.js
+++ src/findKey.js
@@ -1,22 +1,6 @@
-const assertEqual = require('./assertEqual');
-
 const findKey = function(object, callback) {
   const index = Object.values(object).map((value => callback(value))).findIndex(elm => elm === true);
   return index === -1 ? undefined : Object.keys(object)[index];
 };
 
-console.log('findKey tests --');
-const result1 = findKey({
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-}, x => x.stars === 2);
-
-console.log(assertEqual(result1, 'noma'));
-
-const result2 = findKey({ "Blue Hill": { stars: 1 } }, x => x.stars === 2);
-
-console.log(assertEqual(result2, undefined));
+module.exports = findKey;
---
+++ test/findKeyTest.js
@@ -0,0 +1,23 @@
+const { assert } = require('chai');
+const findKey = require('../src/findKey');
+
+describe('findKey test', () => {
+  it('should return a proper key for given an object and a calllback', () => {
+    const stars = {
+      "Blue Hill": { stars: 1 },
+      "Akaleri":   { stars: 3 },
+      "noma":      { stars: 2 },
+      "elBulli":   { stars: 3 },
+      "Ora":       { stars: 2 },
+      "Akelarre":  { stars: 3 }
+    };
+    const actual = findKey(stars, x => x.stars === 2);
+    assert.strictEqual(actual, 'noma');
+  });
+
+  it('should return undefined for no satisfied callback', () => {
+    const stars = { "Blue Hill": { stars: 1 } };
+    const actual = findKey(stars, x => x.stars === 2);
+    assert.isUndefined(actual);
+  });
+});
