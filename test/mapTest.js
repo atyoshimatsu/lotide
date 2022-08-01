1b2fe6bcbd65923da4437e0b157b9921416973c1
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-08-01 13:00:20 -0700

    modified map test

--- test/mapTest.js
+++ test/mapTest.js
@@ -3,7 +3,7 @@ const map = require('../src/map');
 
 describe('map test', () => {
   it('should return proper array for given array and callback', () => {
-    const actual = map('ground', 'control', 'to', 'major', 'tom', word => word[0]);
+    const actual = map(['ground', 'control', 'to', 'major', 'tom'], word => word[0]);
     const expected = ['g', 'c', 't', 'm', 't'];
     assert.deepEqual(actual, expected);
   });
