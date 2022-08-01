36911bd3ddef1e2b2fa64341f0078cb7cb8b9e7f
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-08-01 13:08:21 -0700

    implemented without test

--- src/without.js
+++ src/without.js
@@ -1,5 +1,3 @@
-const assertArraysEqual = require('./assertArraysEqual');
-
 const without = function(arr, rmv) {
   if (!Array.isArray(arr) || !Array.isArray(rmv)) {
     throw new Error('The argument is not Array');
@@ -7,13 +5,4 @@ const without = function(arr, rmv) {
   return arr.filter(elm => !rmv.includes(elm));
 };
 
-console.log('without tests --');
-console.log(assertArraysEqual(without([1, 2, 3, 1], [1]), [2, 3]));
-console.log(assertArraysEqual(without([1, 2, 3], [1, 4]), [2, 3]));
-console.log(assertArraysEqual(without([1, 2, 3], []), [1, 2, 3]));
-console.log(assertArraysEqual(without(['1', '2', '3'], [1, 2, '3']), ['1', '2']));
-
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-console.log(assertArraysEqual(words, ["hello", "world", "lighthouse"]));
+module.exports = without;
---
+++ test/withoutTest.js
@@ -0,0 +1,28 @@
+const { assert } = require('chai');
+const without = require('../src/without');
+
+describe('without test', () => {
+  it('should return [2, 3] for [1, 2, 3, 1], [1]', () => {
+    const actual = without([1, 2, 3, 1], [1]);
+    const expected = [2, 3];
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should return [2, 3] for [1, 2, 3], [1, 4]', () => {
+    const actual = without([1, 2, 3], [1, 4]);
+    const expected = [2, 3];
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should return [1, 2, 3] for [1, 2, 3], []', () => {
+    const actual = without([1, 2, 3], []);
+    const expected = [1, 2, 3];
+    assert.deepEqual(actual, expected);
+  });
+
+  it('should NOT modified original array', () => {
+    const original = [1, 2, 3]
+    without(original, [2, 3]); // returns [1]
+    assert.deepEqual(original, [1, 2, 3]);
+  });
+});
